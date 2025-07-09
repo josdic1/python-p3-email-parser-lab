15fb30f6dd05a5127ba05cf70b75f164e5ab8b6e
josdic1 <joshuadicker@gmail.com>
2025-07-09 10:41:00 -0400

    completed

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,17 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, text):
+        self._text = text
+
+    def parse(self):
+        parts = re.split(r'[,\s]+', self._text)
+        parts = re.findall(r'[\w\.-]+@[\w\.-]+\.\w{2,}', self._text)
+        return sorted(set(parts))
+    
+
+    
+
+    
+
+    
